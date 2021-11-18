4ac12850d580883aaeb842799518c0f8a4e45086
“[negar <miralaienegar@gmail.com>
2021-11-18 21:25:23 +0330

    added functions to avoid multiple answering

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-let allTbodyRow = document.querySelectorAll('tbody tr');
-
+let allTbodyRow = document.querySelectorAll("tbody tr");
 Array.from(allTbodyRow).forEach(function (eachTbodyRow) {
-    eachTbodyRow.addEventListener("click", (e) => {
-        radioIconSrc = eachTbodyRow.querySelector(".radioIcon").src;
-        console.log(radioIconSrc);
-        if (radioIconSrc.includes("grayCircle")) {
-          eachTbodyRow.querySelector(".radioIcon").src =
-            "../images/greenCheckedCircle.png";
-        }
-        console.log(eachTbodyRow.querySelector(".radioIcon").src);
-
-    });
+  eachTbodyRow.addEventListener("click", (e) => {
+    radioIconSrc = eachTbodyRow.querySelector(".radioIcon").src;
+    indexOfSelected = indexOfSelectedAnswer(allTbodyRow);
+    if (indexOfSelected <= allTbodyRow.length) {
+      toggleRadioIcon(allTbodyRow[indexOfSelected]);
+    }
+    toggleRadioIcon(eachTbodyRow);
+  });
 });
+
+function toggleRadioIcon(eachTbodyRow) {
+  if (eachTbodyRow.querySelector(".radioIcon").src.includes("grayCircle")) {
+    eachTbodyRow.querySelector(".radioIcon").src =
+      "../images/greenCheckedCircle.png";
+  } else {
+    eachTbodyRow.querySelector(".radioIcon").src = "../images/grayCircle.png";
+  }
+}
+
+function indexOfSelectedAnswer(allTbodyRow) {
+  for (i = 0; i < allTbodyRow.length; i++) {
+    if (
+      allTbodyRow[i]
+        .querySelector(".radioIcon")
+        .src.includes("greenCheckedCircle")
+    ) {
+      return i;
+    }
+  }
+}
 
 const correctAnswers = [];
